59c74445419216808a3d414abc5593b93192b6e9
stepney141 <stepney141andi.c.i@gmail.com>
2022-02-27 11:28:55 +0900

    test: added perft tests for checker variants

--- examples/international-checkers/perft.js
+++ examples/international-checkers/perft.js
@@ -1,6 +1,7 @@
 import { games } from "../../src/dagaz-model.js";
-import "./chess-dagaz.js";
-import "./chess-dagaz-invariant.js";
+import "./international-checkers-dagaz.js";
+import "./international-checkers-dagaz-promotion.js";
+import "./maximal-captures-dagaz.js";
 
 /**
  * Search the game tree starting from the initial positiion, 
@@ -41,34 +42,8 @@ const main = function (depth, design = games.model.getDesign()) {
   console.timeEnd(`perft ${depth}`);
 };
 
-for (let i = 1; i <= 4; i++){
+for (let i = 1; i <= 10; i++){
   main(i);
 }
 
 console.log("==========");
-
-/* 
-output examples (when 1 <= depth <= 6)
-===============
-
-$ node perft.test.js
-Enumerate Nodes, depth = 1
-result:  20
-perft 1: 25.931ms
-Enumerate Nodes, depth = 2
-result:  400
-perft 2: 105.393ms
-Enumerate Nodes, depth = 3
-result:  8902
-perft 3: 504.85ms
-Enumerate Nodes, depth = 4
-result:  197281
-perft 4: 9.437s
-Enumerate Nodes, depth = 5
-result:  4865609
-perft 5: 4:38.972 (m:ss.mmm) 
-Enumerate Nodes, depth = 6
-result:  119060470
-perft 6: 1:43:10.049 (h:mm:ss.mmm)
-
-*/
