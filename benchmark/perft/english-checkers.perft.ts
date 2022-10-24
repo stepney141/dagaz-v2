15a0577451e05dd24adcd8bfdc14039204c2b07f
stepney141 <stepney141andi.c.i@gmail.com>
2022-10-24 13:32:19 +0900

    test: fixed perft results

--- benchmark/perft/english-checkers.perft.ts
+++ benchmark/perft/english-checkers.perft.ts
@@ -6,7 +6,7 @@ import type { TBoard } from "../../src/core";
  * @link https://damforum.nl/bb3/viewtopic.php?t=2822#p80038
  */
 const PERFT_RESULTS = [
-    1, 7, 49, 302, 1469, 7482, 37986, 179740, 845931, 3963680, 18391564
+    1, 7, 49, 302, 1469, 7361, 36768, 179740, 845931, 3963680, 18391564, 85242128, 388617999
 ];
 
 /**
@@ -50,7 +50,7 @@ const main = function (depth: number) {
     console.timeEnd(`perft ${depth}`);
 };
 
-for (let i = 1; i <= 13; i++) {
+for (let i = 1; i <= 11; i++) {
     main(i);
     console.log("----------");
 }
