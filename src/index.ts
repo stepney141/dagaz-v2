8b3545733e92da92f8128e53b29883236011603d
stepney141 <stepney141andi.c.i@gmail.com>
2022-11-25 12:47:48 +0900

    feat: added entrypoint of src

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * as board from "./board";
+export * as design from "./design";
+export * as dsl from "./move_context";
+export * as move from "./move";
+export * as piece from "./piece";
+export * as tt from "./tt";
