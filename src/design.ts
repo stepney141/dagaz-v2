bcd30021a1245f7816be971832a7e245851768db
stepney141 <stepney141andi.c.i@gmail.com>
2022-12-29 16:34:34 +0900

    refactor: removed '_.range', fixed a type annotation

--- src/design.ts
+++ src/design.ts
@@ -206,7 +206,7 @@ export class TGameRule {
       if (this.boardConnectionGraph.length == 0 && name != "start") {
         //when the locations list is empty, defines the origin of the coordinates
         this.locationNames.push("start");
-        this.boardConnectionGraph.push(_.range(locationDelta.length).fill(0));
+        this.boardConnectionGraph.push(range({ start: 1, stop: locationDelta.length }).fill(0));
       }
       this.locationNames.push(name);
       this.boardConnectionGraph.push(locationDelta);
@@ -632,7 +632,7 @@ export class TDesign {
     return loc;
   }
 
-  dirToString(dir: DirectionID): null | DirectionName {
+  dirToString(dir: DirectionID): DirectionName {
     if (this.directionNames[dir] === undefined) {
       return "?";
     }
