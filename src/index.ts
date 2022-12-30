f7b19ea1c169037283501268442e50a78b8b2703
stepney141 <stepney141andi.c.i@gmail.com>
2022-12-31 00:19:03 +0900

    feat: Export TGameRule as an ESModule

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * as board from "./board";
 export * as design from "./design";
-export * as dsl from "./move_context";
+export * as gamerule_dsl from "./game_rule";
+export * as piecemove_dsl from "./move_context";
 export * as move from "./move";
 export * as piece from "./piece";
 export * as tt from "./tt";
